391fa0daecf29186cf6a72c8b8ea53e2585c4a89
Marcin Pudełek <mrcnpdlk@gmail.com>
2020-09-28 23:12:46 +0200

    zabawa

--- www/index.php
+++ www/index.php
@@ -2,7 +2,11 @@
 
 declare(strict_types=1);
 
+use Tracy\Debugger;
+
 require __DIR__ . '/../vendor/autoload.php';
+
+#Debugger::enable(Debugger::DEVELOPMENT);
 
 Mrcnpdlk\ROD\App\Bootstrap::boot()
 	->createContainer()
